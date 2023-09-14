54a075e0b9d4071dc54ef7e523f79db2164a4349
Marcos Oliveira <marcosfatec2014@gmail.com>
2023-09-14 05:51:42 -0300

    Card Product Settings

--- store/views.py
+++ store/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from product.models import Product
 
 def product_list(request):
+    title_page = "Products List"
     products = Product.objects.all()
-    return render(request, 'store/product/pages/product_list.html', {'products': products})
+    return render(request, 'store/product/pages/product_list.html', {'products': products, 'title_page': title_page})
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
