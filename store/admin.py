9959f309aa8f9ba47e0f2396cbf6455e722d3db5
Marcos Oliveira <marcosfatec2014@gmail.com>
2023-09-17 20:09:09 -0300

    Delete model CustomerTestimonial

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import CustomerTestimonial
-
-admin.site.register(CustomerTestimonial)
--- store/models.py
+++ store/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-class CustomerTestimonial(models.Model):
-    name = models.CharField(max_length=100)
-    content = models.TextField()
-    image = models.ImageField(upload_to='testimonials/')
-
-    def __str__(self):
-        return self.name
